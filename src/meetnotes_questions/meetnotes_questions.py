db8c84b2b9cc05268c75a1b87e3e66963f5f861d
Joseph Turian <turian@gmail.com>
2023-07-24 21:47:38 +0200

    Trying to make async

--- src/meetnotes_questions/meetnotes_questions.py
+++ src/meetnotes_questions/meetnotes_questions.py
@@ -1,3 +1,4 @@
+import asyncio
 import hashlib
 import json
 import os
@@ -15,45 +16,61 @@ load_dotenv()  # take environment variables from .env.
 enc = tiktoken.get_encoding("cl100k_base")
 
 
+# Assume get_question is an asynchronous function that gets the question.
+async def get_question(processed_content):
+    # Code to get the question goes here...
+    print(f"Question: Really?")
+
+
 class FileWatcher(FileSystemEventHandler):
     def __init__(self):
         self.file_hash = {}
 
-    def on_modified(self, event):
-        file_path = event.src_path
-        print(f"Modification detected: {file_path}", file=sys.stderr)
-        if not os.path.isdir(file_path):
-            self.process_file(file_path)
-
-    def on_created(self, event):
-        if not event.is_directory:
-            print(f"New file created: {event.src_path}", file=sys.stderr)
-            self.on_modified(event)
-
-    def on_deleted(self, event):
-        print(f"File deleted: {event.src_path}", file=sys.stderr)
-
-    def process_file(self, file_path):
+    async def process_file(self, file_path):
         file_hash = hashlib.md5()
-
         file_content = self.read_file(file_path)
         file_hash.update(file_content)
 
         if self.file_hash.get(file_path, None) != file_hash.digest():
             self.file_hash[file_path] = file_hash.digest()
-            print("\n\n")
             print(f"File change detected: {file_path}", file=sys.stderr)
-            processed_content = self.process_text(file_content)
-            print(
-                f"Processed content: {json.dumps(processed_content, indent=2)}",
-                file=sys.stderr,
-            )
-            print("\n\n")
+
+        processed_content = self.process_text(file_content)
+        print(
+            f"Processed content: {json.dumps(processed_content, indent=2)}",
+            file=sys.stderr,
+        )
+
+        # fire off an *async* call to get_question
+        asyncio.create_task(get_question(processed_content))
 
     def read_file(self, file_path):
         with open(file_path, "rb") as f:
             buffer = f.read()
         return buffer
+
+    async def on_modified(self, event):
+        try:
+            file_path = event.src_path
+            print(f"Modification detected: {file_path}", file=sys.stderr)
+            if not os.path.isdir(file_path):
+                await self.process_file(file_path)
+        except Exception as e:  # catch the error
+            print(f"Exception occurred in method on_modified: {repr(e)}")
+
+    async def on_created(self, event):
+        try:
+            if not event.is_directory:
+                print(f"New file created: {event.src_path}", file=sys.stderr)
+                await self.on_modified(event)
+        except Exception as e:  # catch the error
+            print(f"Exception occurred in method on_created: {repr(e)}")
+
+    def on_deleted(self, event):
+        try:
+            print(f"File deleted: {event.src_path}", file=sys.stderr)
+        except Exception as e:  # catch the error
+            print(f"Exception occurred in method on_deleted: {repr(e)}")
 
     def process_text(self, file_content):
         text = file_content.decode(
@@ -90,10 +107,13 @@ def parse_conversation(text):
     return conversation
 
 
+# Below is the function:
 def begin_watching(directory_path):
+    loop = asyncio.get_event_loop()
     observer = Observer()
     print(f"Starting to watch: {directory_path}", file=sys.stderr)
-    observer.schedule(FileWatcher(), directory_path, recursive=True)
+    file_watcher = FileWatcher()
+    observer.schedule(file_watcher, directory_path, recursive=True)
     observer.start()
 
     try:
@@ -113,16 +133,6 @@ def main():
     assert os.path.isdir(absolute_dir_path), f"Directory not found: {absolute_dir_path}"
     begin_watching(absolute_dir_path)
 
-
-text = """
-[speaker name]: paragraph 1
-
-paragraph 2
-
-[speaker name 2]: paragraph 3
-
-here's a lot of information that should name be part of the speaker stuff: cool
-"""
 
 if __name__ == "__main__":
     main()
