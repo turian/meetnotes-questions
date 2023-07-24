e926bf9099a4056ce978a50d10a147ee1b115948
Joseph Turian <turian@gmail.com>
2023-07-24 23:37:48 +0200

    Only look for new files

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="meetnotes_questions",
+    version="0.1.9",
+    description="GPT plugin for asking intelligent questions on live meeting notes",
+    author="Joseph Turian",
+    author_email="lastname@gmail.com",
+    url="https://github.com/pypa/meetnotes-questions",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "python-dotenv",
+        "watchdog",
+        "tiktoken",
+        "openai",
+        "asyncio",
+    ],
+    entry_points={
+        "console_scripts": [
+            "meetnotes_questions=meetnotes_questions.main:main",
+        ],
+    },
+)
