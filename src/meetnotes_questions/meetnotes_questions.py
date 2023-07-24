156f17f0a5a71f67f504d1c7d69135c13784c759
Joseph Turian <turian@gmail.com>
2023-07-24 21:06:06 +0200

    Clean up

--- src/meetnotes_questions/meetnotes_questions.py
+++ src/meetnotes_questions/meetnotes_questions.py
@@ -11,8 +11,7 @@ load_dotenv()  # take environment variables from .env.
 
 
 class FileWatcher(FileSystemEventHandler):
-    def __init__(self, callback):
-        self.callback = callback
+    def __init__(self):
         self.file_hash = {}
 
     def on_modified(self, event):
@@ -32,33 +31,31 @@ class FileWatcher(FileSystemEventHandler):
     def process_file(self, file_path):
         file_hash = hashlib.md5()
 
-        with open(file_path, "rb") as f:
-            buffer = f.read()
-        file_hash.update(buffer)
+        file_content = self.read_file(file_path)
+        file_hash.update(file_content)
 
         if self.file_hash.get(file_path, None) != file_hash.digest():
             self.file_hash[file_path] = file_hash.digest()
             print(f"File change detected: {file_path}", file=sys.stderr)
+            processed_content = self.process_text(file_content)
+            print(f"Processed content: {processed_content}", file=sys.stderr)
 
-            if callable(self.callback):
-                self.callback(file_path)
+    def read_file(self, file_path):
+        with open(file_path, "rb") as f:
+            buffer = f.read()
+        return buffer
 
-
-def read_file(file_path):
-    with open(file_path, "r") as file:
-        text = file.read()
-    return process_text(text)
-
-
-def process_text(text):
-    result = text.upper()
-    return result
+    def process_text(self, file_content):
+        text = file_content.decode(
+            "utf-8"
+        )  # decoding required to convert binary data to text
+        return text
 
 
 def begin_watching(directory_path):
     observer = Observer()
     print(f"Starting to watch: {directory_path}", file=sys.stderr)
-    observer.schedule(FileWatcher(read_file), directory_path, recursive=True)
+    observer.schedule(FileWatcher(), directory_path, recursive=True)
     observer.start()
 
     try:
@@ -70,9 +67,7 @@ def begin_watching(directory_path):
 
 
 def main():
-    openai_token = os.environ.get(
-        "OPENAI_TOKEN"
-    )  # Recommended to use get in case the variable is not in the environment
+    openai_token = os.environ.get("OPENAI_TOKEN")
     assert openai_token, "OPENAI_TOKEN not found in the environment variables."
     notes_directory = "~/notes"
     absolute_dir_path = os.path.expanduser(notes_directory)
